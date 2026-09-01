8cfc72dfe2e4591c169fcfe83fca58310097af08
wzzhuz <49175476+wzzhuz@users.noreply.github.com>
2026-09-01 10:23:11 +0800

    fix: 补充 Arrangement 导入

--- app/src/main/java/com/zwz/lifelog/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/zwz/lifelog/ui/settings/SettingsScreen.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
